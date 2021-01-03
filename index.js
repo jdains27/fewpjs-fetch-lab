4d3939a5b9b6776bc51045e60b89f7a1f4125200
jdains27 <dains.jordan2@gmail.com>
2021-01-03 10:16:59 -0600

    commit tree 5a2839456c11d20c9fb19bae9c46e65b9661308c

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-function fetchBooks() {
+const Url = 'https://anapioficeandfire.com/api/books';
 
+const fetchBooks = () => {
+  return fetch(Url)
+    .then( (response) => response.json() )
+    .then( (data) => renderBooks(data) );
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
+const renderBooks = (books) => {
+  const main = document.querySelector('main');
+  books.forEach((book) => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+document.addEventListener('DOMContentLoaded', () => {
+  fetchBooks();
+});
